6bbe24209e8d866dc2d44a5426be77fe9edc5e81
Thomas (Noit) <tba@live.dk>
2020-09-13 17:28:37 +0200

    Add project files.

---
+++ ObligatoryClassLibrary/Cykel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatoryClassLibrary
+{
+    public class Cykel
+    {
+        private int _id;
+        private string _farve;
+        private decimal _pris;
+        private int _gear;
+
+        public Cykel(int id, string farve, decimal pris, int gear)
+        {
+            Id = id;
+            Farve = farve;
+            Pris = pris;
+            Gear = gear;
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Farve
+        {
+            get => _farve;
+            set
+            {
+                if(value.Length < 2) { throw new ArgumentOutOfRangeException("_farve", "_farve must be at least 2 letters");}
+                _farve = value;
+            }
+        }
+
+        public decimal Pris
+        {
+            get => _pris;
+            set
+            {
+                if (value < 0) { throw new ArgumentOutOfRangeException("_pris", "_pris cannot be negative."); }
+                _pris = value;
+            }
+        }
+
+        public int Gear
+        {
+            get => _gear;
+            set
+            {
+                if(value > 32 || value < 3) { throw new ArgumentOutOfRangeException("_gear", "_gear must be no less than 3 and no more than 32."); }
+                _gear = value;
+            }
+        }
+    }
+}
---
+++ ObligatoryClassLibraryTests/CykelTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObligatoryClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatoryClassLibraryTests
+{
+    [TestClass()]
+    public class CykelTests
+    {
+        private static readonly Cykel _cykel = new Cykel(1, "Rd", (decimal)2499.95, 7);
+
+        [TestInitialize]
+        public void Init()
+        {
+            _cykel.Id = 1;
+            _cykel.Farve = "Rd";
+            _cykel.Pris = 2499;
+            _cykel.Gear = 7;
+        }
+
+        [TestMethod()]
+        public void IdTest()
+        {
+            Assert.IsInstanceOfType(_cykel.Id, typeof(int), "Invalid type for Cykel.Id");
+        }
+
+        [TestMethod]
+        public void FarveTest()
+        {
+            Assert.IsInstanceOfType(_cykel.Farve, typeof(string), "Invalid type for Cykel.Farve");
+
+            Assert.AreEqual(2, _cykel.Farve.Length);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _cykel.Farve = "T");
+        }
+
+        [TestMethod]
+        public void PrisTest()
+        {
+            Assert.AreEqual(2499, _cykel.Pris);
+
+            _cykel.Pris = 0;
+            Assert.AreEqual(0, _cykel.Pris);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _cykel.Pris = -1);
+        }
+
+        [TestMethod]
+        public void GearTest()
+        {
+            Assert.AreEqual(7, _cykel.Gear);
+
+            _cykel.Gear = 3;
+            Assert.AreEqual(3, _cykel.Gear);
+
+            _cykel.Gear = 32;
+            Assert.AreEqual(32, _cykel.Gear);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _cykel.Gear = 2);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _cykel.Gear = 33);
+        }
+    }
+}
